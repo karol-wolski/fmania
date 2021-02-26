00bae9b01faf7d1bd08520f70674fdf133461175
Karol Wolski <karolwolski04@gmail.com>
2021-02-27 00:31:56 +0100

    feat: infobox

--- src/shared/Colors.ts
+++ src/shared/Colors.ts
@@ -2,6 +2,7 @@ export const Colors = {
   white100: '#fff',
   black100: '#211f1f',
   black200: '#222220',
+  black300: '#353333',
   blue100: '#889bff',
   blue200: '#506BFA',
   blue300: '#5558DD',
