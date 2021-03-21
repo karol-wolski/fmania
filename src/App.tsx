3530680059e6e1515012adf35fd1ec2ff2b73c79
Karol Wolski <karolwolski04@gmail.com>
2021-03-21 16:47:45 +0100

    feat: breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,23 @@ import Home from './views/Home/Home'
 import Login from './views/Login/Login'
 import ResetPassword from './views/ResetPassword/ResetPassword'
 import Register from './views/Register/Register'
+import Breadcrumbs from './shared/Breadcrumbs/Breadcrumbs'
+import { SITE_WITHOUT_BREADCRUMBS } from '../src/constants/constants'
+
+const displayBreadcrumbs = () => {
+  const windowLocationPathname = window.location.pathname
+  const display = !SITE_WITHOUT_BREADCRUMBS.includes(windowLocationPathname)
+  return display
+}
 
 const App = () => {
+  const isBreadcrumbsShow = displayBreadcrumbs()
   return (
     <>
       <Router>
         <Navigation />
         <Header />
+        {isBreadcrumbsShow && <Breadcrumbs />}
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
