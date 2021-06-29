f446e095bed1d7f3ac22a121caa2bc3f047d6119
Karol Wolski <karolwolski04@gmail.com>
2021-06-29 21:26:28 +0200

    feat: Integration login form with backend

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import LoginForm from '../../components/Forms/LoginForm/LoginForm'
+import isAuthorized from '../../helpers/isAuthorized'
 
 const Login: React.FC = () => {
-  return <LoginForm />
+  return isAuthorized() ? <Redirect to="/" /> : <LoginForm />
 }
 
 export default Login
