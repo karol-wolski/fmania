09f89da25808bc3dedecc1d9db99649b3e413d36
Karol Wolski <poczta@karol-wolski.pl>
2021-06-30 22:43:56 +0200

    fix: Log out the user when the token is invalid on the user orders page during the request to the api (#41)

--- src/views/MyProfile/Orders/Orders.tsx
+++ src/views/MyProfile/Orders/Orders.tsx
@@ -3,30 +3,42 @@ import ProfileWithSidebar from '../../../templates/ProlileWithSidebar/ProfileWit
 import OrdersList from '../../../components/OrdersList/OrdersList'
 import { fetchAsync } from '../../../helpers/fetch'
 import { Span } from './Orders.style'
+import { removeFromLocalStorage } from '../../../helpers/localStorage'
+import { Redirect } from 'react-router-dom'
 
 const Orders: React.FC = () => {
   const [orders, setOrders] = useState([])
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const [inNotLogged, setIsNotLogged] = useState<boolean>(false)
 
   useEffect(() => {
     fetchAsync('user/orders', 'GET').then(response => {
-      setOrders(response)
-      setIsLoaded(true)
+      if (response.statusCode === 401) {
+        setIsNotLogged(true)
+        removeFromLocalStorage('authToken')
+      } else {
+        setOrders(response)
+        setIsLoaded(true)
+      }
     })
   }, [])
   return (
     <>
-      {isLoaded && (
-        <ProfileWithSidebar title="Orders">
-          {orders.length > 0 ? (
-            <OrdersList orders={orders} />
-          ) : (
-            <>
-              <Span>You do not have any orders!</Span>
-              <Span> Do your first shopping :)</Span>
-            </>
-          )}
-        </ProfileWithSidebar>
+      {inNotLogged ? (
+        <Redirect to="/login" />
+      ) : (
+        isLoaded && (
+          <ProfileWithSidebar title="Orders">
+            {orders.length > 0 ? (
+              <OrdersList orders={orders} />
+            ) : (
+              <>
+                <Span>You do not have any orders!</Span>
+                <Span> Do your first shopping :)</Span>
+              </>
+            )}
+          </ProfileWithSidebar>
+        )
       )}
     </>
   )
