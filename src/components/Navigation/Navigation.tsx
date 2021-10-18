f999135843a2cc094aaa2217134b2d72c725ae8f
Karol Wolski <karolwolski04@gmail.com>
2021-10-18 12:23:56 +0200

    refactor: remove tags select from navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,15 +1,5 @@
 import React, { useState } from 'react'
-import {
-  Nav,
-  NavWrapper,
-  NavList,
-  NavItem,
-  NavLink,
-  MenuButton,
-  SelectGroup,
-  Select,
-  Basket,
-} from './Navigation.styles'
+import { Nav, NavWrapper, NavList, NavItem, NavLink, MenuButton, SelectGroup, Basket } from './Navigation.styles'
 import { IoMdClose, IoMdBasket } from 'react-icons/io'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
@@ -48,14 +38,6 @@ const Navigation: React.FC = () => {
           </NavItem>
         </NavList>
         <SelectGroup isVisible={isMobileMenuOpen}>
-          <Select name="languages" id="languages">
-            <option value="en">EN</option>
-            <option value="pl">PL</option>
-          </Select>
-          <Select name="currency" id="currency">
-            <option value="usd">USD</option>
-            <option value="pln">PLN</option>
-          </Select>
           <Basket to="/cart">
             <IoMdBasket />
           </Basket>
